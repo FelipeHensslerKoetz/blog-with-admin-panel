9699ce656b3d20b9d633f1cff7cffdcb65ad2173
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-09 14:13:23 -0300

    chore: cetegory model

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const connection = require("./database/connection");
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
 
+// View Engine
 app.set("view engine", "ejs");
 
+// Static files
 app.use(express.static("public"));
 
+// Accept post requests and JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// DB sync
 connection
   .authenticate()
   .then(() => console.log("Database sync is ok."))
   .catch((err) => console.log(err));
 
+// Routers
 app.use("/", categoriesController);
 app.use("/", articlesController);
 
@@ -24,6 +29,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Server initilization
 app.listen(3000, (err) => {
   if (err) console.log(err);
   console.log("Server is running on 3000 port");
