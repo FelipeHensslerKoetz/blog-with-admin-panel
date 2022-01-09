74886ba922810bb38295fc38647a2008fbf81a40
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-09 15:36:54 -0300

    feat: create category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/categories", (req, res) => {
-  res.send("Categories route");
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
-  res.send("Create a category as admin");
+  res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+  const { title } = req.body;
+
+  if (title === undefined) res.redirect("/admin/categories/new");
+
+  Category.create({
+    title: title,
+    slug: slugify(title),
+  })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
