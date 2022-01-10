2839c28e3f58bb1916a819fcae1089615130c31d
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-09 22:14:23 -0300

    feat: delete and edit a category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const Category = require("./Category");
 const slugify = require("slugify");
 
-router.get("/admin/categories/new", (req, res) => {
-  res.render("admin/categories/new");
+router.get("/admin/categories", (req, res) => {
+  Category.findAll()
+    .then((categories) => {
+      res.render("admin/categories/index", { categories });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.post("/categories/save", (req, res) => {
+router.post("/admin/categories", (req, res) => {
   const { title } = req.body;
 
   if (title === undefined) res.redirect("/admin/categories/new");
@@ -16,16 +20,62 @@ router.post("/categories/save", (req, res) => {
     title: title,
     slug: slugify(title),
   })
-    .then(() => res.redirect("/admin/categories/index"))
+    .then(() => res.redirect("/admin/categories"))
     .catch((err) => console.log(err));
 });
 
-router.get("/admin/categories", (req, res) => {
-  Category.findAll()
-    .then((categories) => {
-      res.render("admin/categories/index", { categories });
-    })
-    .catch((err) => console.log(err));
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
+});
+
+router.post("/admin/categories/:categoryId/delete", (req, res) => {
+  const { categoryId } = req.params;
+
+  if (categoryId !== null && !isNaN(categoryId)) {
+    Category.destroy({ where: { id: categoryId } })
+      .then(() => {
+        res.redirect("/admin/categories");
+      })
+      .catch((err) => res.redirect("/admin/categories"));
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories/:categoryId/edit", (req, res) => {
+  const { categoryId } = req.params;
+
+  if (categoryId !== undefined && !isNaN(categoryId)) {
+    Category.findByPk(categoryId)
+      .then((category) => {
+        res.render("admin/categories/edit", { category });
+      })
+      .catch((err) => res.redirect("/admin/categories"));
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.post("/admin/categories/:categoryId/edit", (req, res) => {
+  const { title } = req.body;
+  const { categoryId } = req.params;
+
+  if (!isNaN(categoryId)) {
+    Category.update(
+      { title, slug: slugify(title) },
+      {
+        where: { id: categoryId },
+      }
+    )
+      .then(() => {
+        res.redirect("/admin/categories");
+      })
+      .catch((err) => {
+        res.redirect("/admin/categories");
+      });
+  } else {
+    res.redirect("/admin/categories");
+  }
 });
 
 module.exports = router;
