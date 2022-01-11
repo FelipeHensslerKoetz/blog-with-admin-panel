bc3aa8a9fe1b9d5729fe74f6a35f77ac7ee86b39
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-11 20:38:28 -0300

    feat: articles index, new and delete actions

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const Article = require("./Article");
+const Category = require("../categories/Category");
+const slugify = require("slugify");
+const { route } = require("express/lib/application");
 
 router.get("/admin/articles", (req, res) => {
-  res.render("admin/articles/index");
+  Article.findAll({ include: [{ model: Category }] })
+    .then((articles) => {
+      res.render("admin/articles/index", { articles });
+    })
+    .catch((err) => console.log(err));
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll()
+    .then((categories) => res.render("admin/articles/new", { categories }))
+    .catch((err) => res.redirect("/admin/articles"));
 });
 
-router.post("/admin/articles", (req, res) => {});
+router.post("/admin/articles", (req, res) => {
+  const { title, body, category } = req.body;
+
+  Article.create({ title, body, slug: slugify(title), categoryId: category })
+    .then(() => res.redirect("/admin/articles"))
+    .catch((err) => console.log(err));
+});
+
+router.post("/admin/articles/:articleId/delete", (req, res) => {
+  const { articleId } = req.params;
+
+  if (!isNaN(articleId)) {
+    Article.destroy({ where: { id: articleId } })
+      .then(() => {
+        res.redirect("/admin/articles");
+      })
+      .catch((err) => {
+        res.redirect("/admin/articles");
+      });
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
 
 module.exports = router;
